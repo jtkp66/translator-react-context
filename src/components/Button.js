6c732b10eeb4c654aa98c6342bb0667686952732
Jamie Pham <jtkp66@gmail.com>
2019-02-25 09:33:56 -0800

    Added consumer object to button component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends Component {
-    static contextType = LanguageContext;
-
   render() {
-      const text = this.context === 'english' ? 'Submit' : 'Voorleggen'
-    return <button className="ui button primary">{text}</button>;
+    return (
+    <button className="ui button primary">
+      <LanguageContext.Consumer>
+        {(value) => value === 'english' ? 'Submit' : 'Voorleggen'}
+      </LanguageContext.Consumer>
+    </button>
+    );
   }
 }
 
 export default Button;
+
+  // class Button extends Component {
+  //     static contextType = LanguageContext;
+  
+  //   render() {
+  //       const text = this.context === 'english' ? 'Submit' : 'Voorleggen'
+  //     return <button className="ui button primary">{text}</button>;
+  //   }
+  // }
+  
+  // export default Button;
+  
