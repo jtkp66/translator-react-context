72b7f98384852d3c79547fed6210a941526c0e31
Jamie Pham <jtkp66@gmail.com>
2019-02-24 22:47:54 -0800

    Added functionality to select language by clicking on the flags.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
   render() {
     return (
       <div className="ui container">
-        App
+        <div>
+          Select A Language
+          <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+        </div>
+        {this.state.language}
       </div>
-    )
+    );
   }
 }
 
